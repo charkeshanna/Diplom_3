0b426a23bfcc0f1ae3a82c436395ffc220219dc9
User Charkes <charkes.hanna@gmail.com>
2026-01-17 12:05:36 +0200

    added steps to LoginPage

--- src/main/java/pom/LoginPage.java
+++ src/main/java/pom/LoginPage.java
@@ -1,14 +1,11 @@
 package pom;
-
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
-
-import static java.time.Duration.ofSeconds;
 
 public class LoginPage {
     private WebDriver driver;
@@ -31,35 +28,46 @@ public class LoginPage {
     //Ссылка восстановить пароль
     private By forgotPasswordLink = By.linkText("Восстановить пароль");
 
+
     // метод ожидания загрузки страницы --- проверить еще раз
+    @Step("Ожидание загрузки страницы входа")
     public void waitForLoadPage(){
         wait.until(ExpectedConditions.visibilityOfElementLocated(loginButton));
     }
+
     //метод клика по линке Зарегистрироваться
+    @Step("Клик по ссылке 'Зарегистрироваться'")
     public void clickRegistrateLink() {
         driver.findElement(registrateLink).click();
     }
+
     //заполняем поле с Email
+    @Step("Ввод email: {email}")
     public void inputEmail (String email) {
         WebElement input = wait.until(ExpectedConditions.elementToBeClickable(emailInputField));
         input.sendKeys(email);
     }
     //заполняем поле с password
+    @Step("Ввод пароля")
     public void inputPassword(String password) {
         WebElement input = wait.until(ExpectedConditions.elementToBeClickable(passwordInputField));
         input.sendKeys(password);
     }
 
     //нажимаем кнопку Войти
+    @Step("Клик по кнопке 'Войти'")
     public void clickLoginButton() {
         wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
     }
 
+    //проверяем отображение кнопки Войти
+    @Step("Проверка отображения кнопки 'Войти'")
     public boolean isLoginButtonDisplayed() {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(loginButton)).isDisplayed();
     }
 
-    //кликаем по линке Восстановить пароль
+    //метод клика по ссылке Восстановить пароль
+    @Step("Клик по ссылке 'Восстановить пароль'")
     public void clickForgotPasswordLink() {
         driver.findElement(forgotPasswordLink).click();
     }
