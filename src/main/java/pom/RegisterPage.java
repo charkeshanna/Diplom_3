099d368e9e5c6feaf3241b7f2863a73e625aba92
User Charkes <charkes.hanna@gmail.com>
2026-01-17 12:15:09 +0200

    added steps to registerPage

--- src/main/java/pom/RegisterPage.java
+++ src/main/java/pom/RegisterPage.java
@@ -1,5 +1,6 @@
 package pom;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -34,34 +35,46 @@ public class RegisterPage {
     private By loginLink = By.linkText("Войти");
 
     // метод ожидания загрузки страницы --- проверить еще раз
+    @Step("Ожидание загрузки страницы регистрации")
     public void waitForLoadPage(){
         new WebDriverWait(driver, ofSeconds(3))
-                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[text()='Зарегистрироваться']")));
+                .until(ExpectedConditions.visibilityOfElementLocated(registrateButton));
     }
 
     //заполняем поле Имя
+    @Step("Ввод имени: {name}")
     public void inputName(String name) {
         driver.findElement(nameInputField).sendKeys(name);
     }
+
     //заполняем поле с Email
+    @Step("Ввод email: {email}")
     public void inputEmail (String email) {
         driver.findElement(emailInputField).sendKeys(email);
     }
+
+
     //заполняем поле с password
+    @Step("Ввод пароля")
     public void inputPassword(String password) {
         driver.findElement(passwordInputField).sendKeys(password);
     }
 
     //метод клика по кнопке Зарегистрироваться
+    @Step("Клик по кнопке 'Зарегистрироваться'")
     public void clickRegistrationButton() {
         driver.findElement(registrateButton).click();
     }
+
     //получаем сообщение об ошибке
+    @Step("Получение текста сообщения об ошибке пароля")
     public String getPasswordErrorMessage() {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(passwordErrorMessage)).getText();
     }
 
+
     //получаем инфу отображется ли текст ошибки
+    @Step("Проверка отображения сообщения об ошибке пароля")
     public boolean isPasswordErrorDisplayed() {
         try {
             return wait.until(ExpectedConditions.visibilityOfElementLocated(passwordErrorMessage)).isDisplayed();
@@ -70,6 +83,8 @@ public class RegisterPage {
         }
     }
 
+    //метод клика по ссылке Войти
+    @Step("Клик по ссылке 'Войти'")
     public void clickLoginLink() {
         driver.findElement(loginLink).click();
     }
