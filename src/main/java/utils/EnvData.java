21945cca28a0e9f1eb9b1872d391a220ce27de35
User Charkes <charkes.hanna@gmail.com>
2026-01-17 16:55:17 +0200

    minor updates

--- src/main/java/utils/EnvData.java
+++ src/main/java/utils/EnvData.java
@@ -9,9 +9,5 @@ public class EnvData {
     public static String getBaseUrl() {
         return BASE_URL;
     }
-    // Браузер по умолчанию
-    /*public static final String DEFAULT_BROWSER = "chrome";
-    public static String getBrowser() {
-        return System.getProperty("browser", DEFAULT_BROWSER);
-    }*/
+
 }
